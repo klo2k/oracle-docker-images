2905db6ed2fa4c8d781273b6cee552f215c9c2c0
Kok-Yan Lo <klo2k@users.noreply.github.com>
2022-06-30 19:45:56 +1000

    revert

--- OracleDatabase/SingleInstance/dockerfiles/build-oracle-database-image.sh
+++ OracleDatabase/SingleInstance/dockerfiles/build-oracle-database-image.sh
@@ -7,13 +7,6 @@ cd "$(dirname $0)/19.3.0"
 
 # Build 19c Oracle database image
 docker build --pull \
-  --build-arg DB_EDITION=xe \
-  --label org.opencontainers.image.revision="$(git rev-parse HEAD)" \
-  --tag klo2k/oracle-database:19.3.0-xe \
-  --file Dockerfile \
-  .
-
-docker build --pull \
   --build-arg DB_EDITION=ee \
   --label org.opencontainers.image.revision="$(git rev-parse HEAD)" \
   --tag klo2k/oracle-database:19.3.0-ee \
--- OracleDatabase/SingleInstance/dockerfiles/publish-oracle-database-image.sh
+++ OracleDatabase/SingleInstance/dockerfiles/publish-oracle-database-image.sh
@@ -2,6 +2,4 @@
 
 set -eu
 
-docker push klo2k/oracle-database:19.3.0-xe
-
 docker push klo2k/oracle-database:19.3.0-ee
